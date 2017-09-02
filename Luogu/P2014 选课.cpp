a9b89c4c31cf429a96545c9663abc14338c2583c
Yangyu Chen <877318438@qq.com>
2017-09-02 05:27:11 -0500

    将时间复杂度从O(n*m^2)优化成O(n*m)

--- Luogu/P2014 选课.cpp
+++ Luogu/P2014 选课.cpp
@@ -1,38 +1,40 @@
 #include <bits/stdc++.h>
 using std::max;
 int head[305];
-int next[305];
-int to[305];
-int w[305];
+struct Edge {
+	int v,w;
+	int c;//cost
+	int next;
+}e[305];
 int ecnt = 0;
-int m;
-void add(int _u,int _v,int _w) {
-	next[ecnt] = head[_u];
-	head[_u] = ecnt;
-	to[ecnt] = _v;
-	w[ecnt] = _w;
+int f[305][305];
+void AddEdge(int u,int v,int w,int c) {
+	e[ecnt].next = head[u];
+	head[u] = ecnt;
+	e[ecnt].v = v;
+	e[ecnt].w = w;
+	e[ecnt].c = c;
 	ecnt ++;
 }
-int f[305][305];
-void dfs(int n) {
-	for (int e=head[n];e != -1;e=next[e]) {
-		int v = to[e];
-		dfs(v);
-		for (int j=m;j>=1;j--)
-			for (int k=1;k<=j;k++)
-				f[n][j] = max(f[n][j],f[n][j-k]+f[v][k-1]+w[e]);
+void dfs(int cur,int maxp) {//p是当前物品的价格,w是价值(价格*重要度)
+	for (int i=head[cur];i != -1;i = e[i].next) {
+		if (maxp <= e[i].c) continue;
+		for (int j=0;j<=maxp-e[i].c;j++) f[e[i].v][j] = f[cur][j] + e[i].w;
+		dfs(e[i].v,maxp-e[i].c);
+		for (int j=maxp;j>=e[i].c;j--)
+			f[cur][j] = max(f[cur][j],f[e[i].v][j-e[i].c]);
 	}
 }
 int main() {
+	int n,m;
 	memset(head,-1,sizeof(head));
-	int n;
 	scanf("%d%d",&n,&m);
 	for (int i=1;i<=n;i++) {
 		int k,s;
 		scanf("%d%d",&k,&s);
-		add(k,i,s);
+		AddEdge(k,i,s,1);
 	}
-	dfs(0);
+	dfs(0,m);
 	printf("%d\n",f[0][m]);
 	return 0;
 }
