5acebcc76c0822f118a1925eb1a5348838e9ae28
Yangyu Chen <chen_yy@outlook.com>
2017-11-08 07:58:52 -0600

    重写

--- Luogu/P1083 借教室.cpp
+++ Luogu/P1083 借教室.cpp
@@ -1,78 +1,95 @@
+//卡常卡掉一个点，Vijos可过
 #include <bits/stdc++.h>
+const int maxn = 1000005;
+const int maxm = 1000005;
+int a[maxn];
 struct tree{
-	int MIN;//单调减，就写的不那么严谨了
-	int flag;//懒人标记
-}tr[4000005];
-int a[1000005];
-inline int max(int a,int b) {
-	if (a > b) return a;
-	return b;
-}
+	int MIN;
+	int add;//lazy flag
+}tr[maxn<<2];
 inline int min(int a,int b) {
-	if (a < b) return a;
-	return b;
+	if (a > b) return b;
+	return a;
 }
-inline void push_up(int n) {
-	tr[n].MIN = min(tr[n<<1].MIN,tr[n<<1|1].MIN);
+inline void PushUp(int node) {
+	tr[node].MIN = min(tr[node<<1].MIN,tr[node<<1|1].MIN);
 }
-inline void push_down(int n) {
-	tr[n<<1].flag += tr[n].flag;
-	tr[n<<1|1].flag += tr[n].flag;
-	tr[n<<1].MIN += tr[n].flag;
-	tr[n<<1|1].MIN += tr[n].flag;
-	tr[n].flag = 0;
-}
-void build_tree(int n,int l,int r) {
-	if (r > l) {
-		int mid = (l + r) >> 1;
-		build_tree(n<<1,l,mid);
-		build_tree(n<<1|1,mid+1,r);
-		push_up(n);
+inline void build_tree(int node,int L,int R) {
+	if (L == R) {
+		tr[node].MIN = a[L];
+		return;
 	}
-	else tr[n].MIN = a[l];
+	int mid = (L + R) >> 1;
+	build_tree(node<<1,L,mid);
+	build_tree(node<<1|1,mid+1,R);
+	PushUp(node);
 }
-long long update(int l,int r,int d,int L,int R,int n) {//L、R为树节点区间
-	if (l <= L && r >= R) {
-		tr[n].MIN += d;
-		tr[n].flag += d;
-		return tr[n].MIN;
+inline void PushDown(int node) {
+	tr[node<<1].add += tr[node].add;
+	tr[node<<1].MIN += tr[node].add;
+	tr[node<<1|1].add += tr[node].add;
+	tr[node<<1|1].MIN += tr[node].add;
+	tr[node].add = 0;
+}
+inline int query(int node,int l,int r,int L,int R) {
+	if (L == l && R == r) return tr[node].MIN;
+	if (tr[node].add) PushDown(node);
+	int mid = (L + R) >> 1;
+	if (r <= mid) return query(node<<1,l,r,L,mid);
+	if (l > mid) return query(node<<1|1,l,r,mid+1,R);
+	return min(query(node<<1,l,mid,L,mid),query(node<<1|1,mid+1,r,mid+1,R));
+}
+inline void insert(int node,int l,int r,int L,int R,int d) {
+	if (l == L && R == r) {
+		tr[node].add += d;
+		tr[node].MIN += d;
 	}
 	else {
 		int mid = (L + R) >> 1;
-		if (tr[n].flag != 0) push_down(n);
-		if (l <= mid) update(l,r,d,L,mid,n<<1);
-		if (r > mid) update(l,r,d,mid+1,R,n<<1|1);
-		push_up(n);
-		return tr[n].MIN;
+		PushDown(node);
+		if (r <= mid) insert(node<<1,l,r,L,mid,d);
+		else if (l > mid) insert(node<<1|1,l,r,mid+1,R,d);
+		else {
+			insert(node<<1,l,mid,L,mid,d);
+			insert(node<<1|1,mid+1,r,mid+1,R,d);
+		}
+		PushUp(node);
 	}
 }
 inline int read() {
-	int sum = 0;
 	int f = 1;
+	int ret = 0;
 	char c = getchar();
-	while (c < '0' || c > '9') {
-		if (c == '-') f = -1;
+	while (!(c >= '0' && c <='9')) {
+		if (c == '-') f = -f;
 		c = getchar();
 	}
-	while (c >= '0' && c <= '9') {
-		sum = sum * 10 + c - '0';
+	while (c >= '0' && c <='9') {
+		ret *= 10;
+		ret += c - '0';
 		c = getchar();
 	}
-	return sum;
+	return ret * f;
 }
 int main() {
 	int n,m;
-	n=read();
-	m=read();
+	//scanf("%d%d",&n,&m);
+	n = read();
+	m = read();
+	//for (int i=1;i<=n;i++) scanf("%d",&a[i]);
 	for (int i=1;i<=n;i++) a[i] = read();
 	build_tree(1,1,n);
-	for (int i=1;i<=m;i++) {
-		int d=read(),s=read(),j=read();
-		if (update(s,j,-d,1,n,1) < 0) {
-			printf("-1\n%d\n",i);
-			return 0;
-		}
+	int i;
+	for (i=1;i<=m;i++) {
+		int d,s,t;
+		d = read();
+		s = read();
+		t = read();
+		//scanf("%d%d%d",&d,&s,&t);
+		if (query(1,s,t,1,n) >= d) insert(1,s,t,1,n,-d);
+		else break;
 	}
-	printf("0\n");
+	if (i == m+1) printf("0\n");
+	else printf("-1\n%d\n",i);
 	return 0;
 }
