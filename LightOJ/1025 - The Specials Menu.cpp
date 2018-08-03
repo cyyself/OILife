59a7912979bd783fc0172d93e52e484985d5c1d7
Yangyu Chen <cyy@cyyself.name>
2018-08-03 13:34:33 +0800

    Update 1025 - The Specials Menu.cpp

--- LightOJ/1025 - The Specials Menu.cpp
+++ LightOJ/1025 - The Specials Menu.cpp
@@ -14,9 +14,8 @@ int main() {
 		for (int d=2;d<=len;d++) {
 			for (int i=1;i+d-1<=len;i++) {
 				int j = i + d - 1;
-				f[i][j] = f[i][j-1] + f[i+1][j];
-				if (s[i] == s[j]) f[i][j] ++;
-				else f[i][j] -= f[i+1][j-1];
+				f[i][j] = f[i][j-1] + f[i+1][j] - f[i+1][j-1];//容斥
+				if (s[i] == s[j]) f[i][j] += f[i+1][j-1] + 1;//相当于[i+1][j-1]范围的子串都可以前后加相同的字符，所以增加了方案数，然后s[i]、s[j]又可以组合成一个回文串，所以再+1
 			}
 		}
 		printf("Case %d: %lld\n",c,f[1][len]);
