bd5c27216353e811c6d0ab5a066d3ee653c1b6ab
Yangyu Chen <cyy@cyyself.name>
2018-08-14 09:20:59 +0800

    Update 3281 - Dining.cpp

--- POJ/3281 - Dining.cpp
+++ POJ/3281 - Dining.cpp
@@ -1,99 +1,105 @@
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 #include <queue>
 using namespace std;
-#define INF 0x3f3f3f3f
-int n,f,d;
-const int s = 0;
-const int t = 401;
-int head[405];
-int nexte[16005];
-int to[16005];
-int c[16005];
-int ecnt = 0;
-inline void AddEdge(int _from,int _to,int _cap) {
-	//不含反向弧
-	nexte[ecnt] = head[_from];
-	head[_from] = ecnt;
-	to[ecnt] = _to;
-	c[ecnt] = _cap;
+const int INF = 0x3f3f3f3f;
+struct edge {
+	int u,v,f,c;
+	int next;
+}e[40605];
+int head[402];
+int ecnt;
+void _AddEdge(int u,int v,int f,int c) {
+	e[ecnt].u = u;
+	e[ecnt].v = v;
+	e[ecnt].f = f;
+	e[ecnt].c = c;
+	e[ecnt].next = head[u];
+	head[u] = ecnt;
 	ecnt ++;
 }
-inline void AddFlow(int _from,int _to,int _cap) {
-	AddEdge(_from,_to,_cap);
-	AddEdge(_to,_from,0);
-	//debug {
-	//printf("added:%d->%d\n",_from,_to);
-	//debug }
+void AddEdge(int u,int v,int f,int c) {
+	_AddEdge(u,v,f,c);
+	_AddEdge(v,u,0,-c);
 }
-/*
-编号规则：
-s:0
-food从1..f
-drink从101..100+d
-连接f的牛从201..200+n
-连接d的牛从301..300+n
-t:401
-*/
-int dis[405];
-bool bfs() {
+int pre[402];
+int dis[402];
+bool inq[402];
+bool spfa(int s,int t) {
 	memset(dis,0x3f,sizeof(dis));
-	dis[s] = 0;
+	memset(pre,-1,sizeof(pre));
+	memset(inq,false,sizeof(inq));
 	queue <int> q;
 	q.push(s);
+	dis[s] = 0;
+	inq[s] = true;
 	while (!q.empty()) {
-		int cur = q.front();
+		int u = q.front();
 		q.pop();
-		for (int en = head[cur];en != -1; en = nexte[en]) {
-			int v = to[en];
-			if (c[en] != 0 && dis[v] == INF) {
-				dis[v] = dis[cur] + 1;
-				q.push(v);
+		inq[u] = false;
+		for (int i=head[u];i != -1;i = e[i].next) {
+			if (e[i].f > 0 && dis[u] + e[i].c < dis[e[i].v]) {
+				dis[e[i].v] = dis[u] + e[i].c;
+				pre[e[i].v] = i;
+				if (!inq[e[i].v]) {
+					q.push(e[i].v);
+					inq[e[i].v] = true;
+				}
 			}
 		}
 	}
 	return dis[t] != INF;
 }
-int dfs(int u,int curflow) {
-	if (u == t) return curflow;
-	if (dis[u] >= dis[t]) return 0;
-	for (int en = head[u]; en != -1;en = nexte[en]) {
-		int v = to[en];
-		int flow;
-		if (c[en] != 0 && dis[v] == dis[u] + 1 && (flow = dfs(v,min(curflow,c[en])))) {
-			c[en] -= flow;
-			c[en ^ 1] += flow;
-			return flow;
+void MCMF(int s,int t,int &f,int &c) {
+	f = 0;
+	c = 0;
+	while (spfa(s,t)) {
+		int curFlow = INF;
+		for (int i=pre[t];i != -1;i = pre[e[i].u]) curFlow = min(curFlow,e[i].f);
+		for (int i=pre[t];i != -1;i = pre[e[i].u]) {
+			e[ i ].f -= curFlow;
+			e[i^1].f += curFlow;
 		}
+		f += curFlow;
+		c += curFlow * dis[t];
 	}
-	return 0;
 }
-int dinic() {
-	int ans = 0;
-	int flow;
-	while(bfs()) while(flow = dfs(s,INF)) ans += flow;
-	return ans;
-}
+/*
+编号规则：
+	源点：0
+	食物：1~100
+	奶牛左：101~200
+	奶牛右：201~301
+	（不拆点可能存在的一种情况是多个食物流向一头牛，然后再从多个饮料流出）
+	饮料：301~400
+	终点：401
+流向：源点->食物->奶牛->饮料->终点
+*/
 int main() {
 	memset(head,-1,sizeof(head));
+	ecnt = 0;
+	int n,f,d;
 	scanf("%d%d%d",&n,&f,&d);
-	for (int i=1;i<=f;i++) AddFlow(0,i,1);
-	for (int i=101;i<=100+d;i++) AddFlow(i,401,1);
-	for (int i=201;i<=200+n;i++) {
+	for (int i=1;i<=f;i++) AddEdge(0,i,1,1);//源点流向食物
+	for (int i=101;i<=100+n;i++) {
 		int fi,di;
 		scanf("%d%d",&fi,&di);
-		AddFlow(i,i+100,1);
-		for (int j=0;j<fi;j++) {
-			int _f;
-			scanf("%d",&_f);
-			AddFlow(_f,i,1);
+		while (fi --) {
+			int tmp;
+			scanf("%d",&tmp);
+			AddEdge(tmp,i,1,1);//食物流向奶牛左
 		}
-		for (int j=0;j<di;j++) {
-			int _d;
-			scanf("%d",&_d);
-			AddFlow(i+100,_d+100,1);
+		AddEdge(i,i+100,1,1);//奶牛左流向奶牛右
+		while (di --) {
+			int tmp;
+			scanf("%d",&tmp);
+			AddEdge(100+i,300+tmp,1,1);//奶牛右流向饮料
 		}
 	}
-	printf("%d\n",dinic());
+	for (int i=301;i<=300+d;i++) AddEdge(i,401,1,1);
+	int flow,cost;
+	MCMF(0,401,flow,cost);
+	printf("%d\n",flow);
 	return 0;
 }
