34e559a27450b5d8b8c84fa8afb3f2f6406ee02a
Yangyu Chen <cyy@cyyself.name>
2018-08-22 10:39:37 +0800

    Create 690C2 - Brain Network (medium).cpp

---
+++ Codeforces/690C2 - Brain Network (medium).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector <int> g[100005];
+bool vis[100005];
+int dis[100005];
+int n;
+void getdis(int u) {
+	vis[u] = true;
+	for (auto v:g[u]) {
+		if (!vis[v]) {
+			dis[v] = dis[u] + 1;
+			getdis(v);
+		}
+	}
+}
+int main() {
+	int m;
+	scanf("%d%d",&n,&m);
+	for (int i=0;i<m;i++) {
+		int u,v;
+		scanf("%d%d",&u,&v);
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	memset(vis,false,sizeof(vis));
+	getdis(1);
+	int root = 0;
+	for (int i=1;i<=n;i++) if (dis[i] > dis[root]) root = i;
+	memset(dis,0,sizeof(dis));
+	memset(vis,false,sizeof(vis));
+	getdis(root);
+	int ans = 0;
+	for (int i=1;i<=n;i++) ans = max(ans,dis[i]);
+	printf("%d\n",ans);
+	return 0;
+}
