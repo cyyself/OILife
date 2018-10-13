1335c18bb3967d105442c3f33c3ecbdee398e61e
Yangyu Chen <cyy@cyyself.name>
2018-10-13 10:26:39 +0800

    Create 1066D - Boxes Packing.cpp

---
+++ Codeforces/1066D - Boxes Packing.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a[200005];
+int main() {
+	int n,m,k;
+	scanf("%d%d%d",&n,&m,&k);
+	for (int i=0;i<n;i++) scanf("%d",&a[i]);
+	int rem = k;
+	int ans = 0;
+	for (int i=n-1;i>=0 && m > 0;i--) {
+		if (rem - a[i] < 0) {
+			rem = k;
+			m --;
+		}
+		if (m <= 0) break;
+		rem -= a[i];
+		ans ++;
+	}
+	printf("%d\n",ans);
+	return 0;
+}
