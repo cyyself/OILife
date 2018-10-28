c3db51859003a361423b67a7eed2aab4fbb8d915
Yangyu Chen <cyy@cyyself.name>
2018-10-28 23:32:05 +0800

    Update 1185 -  炮兵阵地.cpp

--- POJ/1185 -  炮兵阵地.cpp
+++ POJ/1185 -  炮兵阵地.cpp
@@ -1,55 +1,49 @@
-#include <iostream>
+#include <cstdio>
+#include <cstring>
 #include <algorithm>
 using namespace std;
-int s[65];//只有60个合法状态
-int scnt = 0;
+int n,m;
+int tstat;
+inline bool judge(int x) {
+	return !( (x & (x << 1)) | (x & (x << 2)));
+}
+int stat[65];
 int f[105][65][65];
-int matrix[105];
-inline bool judgex(int x) {
-	if (x & (x << 1)) return false;
-	if (x & (x << 2)) return false;
-	return true;
-}
-inline bool judgey(int x,int y) {
-	if (x & y) return false;
-	return true;
-}
-inline int getn(int x) {
-	int ret = 0;
-	while (x) {
-		ret += x & 1;
-		x = x >> 1;
+int scnt;
+int g[105];
+void cal() {
+	for (int m=1;m<=10;m++) {
+		int cnt = 0;
+		for (int i=0;i<(1<<m);i++) if (judge(i)) cnt ++;
+		printf("%d\n",cnt);
 	}
-	return ret;
 }
 int main() {
-	int n,m;
-	cin >> n >> m;
-	for (int i=0;i<n;i++) 
+	//cal();
+	scanf("%d%d",&n,&m);
+	tstat = (1 << m) - 1;
+	for (int i=0;i<(1<<m);i++) if (judge(i)) stat[scnt ++] = i;
+	for (int i=0;i<n;i++) {
 		for (int j=0;j<m;j++) {
-			char t;
-			cin >> t;
-			if (t == 'H') matrix[i] += (1 << j);
+			char c;
+			scanf(" %c",&c);
+			if (c == 'H') g[i] |= (1 << j);
 		}
-	
-	for (int i=0;i<(1<<m);i++) if (judgex(i)) s[scnt++] = i;
-	for (int i=0;i<scnt;i++) if (judgey(s[i],matrix[0])) f[0][i][0] = getn(s[i]);
-	for (int i=1;i<n;i++) {
-		for (int j=0;j<scnt;j++) {
-			if (!judgey(s[j],matrix[i])) continue;
-			int jn = getn(s[j]);
-			for (int k=0;k<scnt;k++) {
-				if (!judgey(s[j],s[k])) continue;
-				for (int l=0;l<scnt;l++) {
-					if (!judgey(s[j],s[l])) continue;
-					f[i][j][k] = max(f[i][j][k],f[i-1][k][l] + jn);
-				}
+	}
+	for (int i=0;i<n;i++) for (int j=0;j<scnt;j++) {
+		if (stat[j] & g[i]) continue;
+		int cnt =  __builtin_popcount(stat[j]);
+		if (i == 0) f[i][j][0] = cnt;
+		else for (int k=0;k<scnt;k++) {
+			if (stat[j] & stat[k]) continue;
+			for (int l=0;l<scnt;l++) {
+				if (stat[j] & stat[l]) continue;
+				f[i][j][k] = max(f[i][j][k],f[i-1][k][l]+cnt);
 			}
 		}
 	}
 	int ans = 0;
-	for (int i=0;i<scnt;i++)
-		for (int j=0;j<scnt;j++) ans = max(ans,f[n-1][i][j]);
-	cout << ans << endl;
+	for (int i=0;i<scnt;i++) for (int j=0;j<scnt;j++) ans = max(ans,f[n-1][i][j]);
+	printf("%d\n",ans);
 	return 0;
 }
