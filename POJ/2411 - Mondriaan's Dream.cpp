1abf7d670dea51debc0070b8322db0378499abea
Yangyu Chen <cyy@cyyself.name>
2018-10-28 20:47:05 +0800

    Update 2411 - Mondriaan's Dream.cpp

--- POJ/2411 - Mondriaan's Dream.cpp
+++ POJ/2411 - Mondriaan's Dream.cpp
@@ -2,39 +2,41 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-int n,m;
-long long f[2][1<<11];
-bool mark[1<<11];
-inline bool judge(int i) {
-	while (i) {
-		if (i & 1) {
-			i >>= 1;
-			if (!(i & 1)) return false;
+bool judge(int stat) {
+	while (stat) {
+		if (stat & 1) {
+			stat >>= 1;
+			if (!(stat & 1)) return false;
 		}
-		i >>= 1;
+		stat >>= 1;
 	}
 	return true;
 }
+bool valid[1<<11];
+int s[150];
+int validcnt;
+long long f[11][1<<11];
 int main() {
-	while (scanf("%d%d",&n,&m) == 2 && (n||m)) {
+	for (int i=0;i<(1<<11);i++) if (judge(i)) {
+		valid[i] = true;
+		s[validcnt ++] = i;
+	}
+	//printf("%d\n",validcnt);
+	int n,m;
+	while (scanf("%d%d",&n,&m) == 2 && n && m) {
 		if (n < m) swap(n,m);
-		//n > m
-		memset(mark,false,sizeof(mark));
-		memset(f[0],0,sizeof(f[0]));
-		for (int j=0;j<(1<<m);j++) if (judge(j)) {
-			f[0][j] = 1;
-			mark[j] = true;
-		}
+		memset(f,0,sizeof(f));
+		int scnt = upper_bound(s,s+validcnt,(1<<m)-1) - s;
+		for (int j=0;j<scnt;j++) f[0][s[j]] = 1;
 		for (int i=1;i<n;i++) {
-			memset(f[i&1],0,sizeof(f[i&1]));
-			for (int j=0;j<(1<<m);j++) 
+			for (int j=0;j<(1<<m);j++)
 				for (int k=0;k<(1<<m);k++) {
-					if ( (j|k) != (1<<m) - 1) continue;
-					if (!mark[j&k]) continue;
-					f[i&1][j] += f[(i&1)^1][k];
+					if ( (j|k) != (1 << m) - 1) continue;
+					if (!valid[j&k]) continue;
+					f[i][j] += f[i-1][k];
 				}
 		}
-		printf("%lld\n",f[(n&1)^1][(1<<m)-1]);
+		printf("%lld\n",f[n-1][(1<<m)-1]);
 	}
 	return 0;
 }
