1584af7a41a89a68884bf824140fa7d2acad4f94
Yangyu Chen <cyy@cyyself.name>
2018-11-21 23:02:24 +0800

    Update 1328 - Radar Installation.cpp

--- POJ/1328 - Radar Installation.cpp
+++ POJ/1328 - Radar Installation.cpp
@@ -1,40 +1,40 @@
 #include <cstdio>
 #include <cmath>
-#include <utility>
 #include <algorithm>
+#include <utility>
 using namespace std;
 const double eps = 1e-5;
 pair <double,double> p[1005];
 int main() {
 	int n;
-	double r;
-	int cas = 1;
-	while (scanf("%d%lf",&n,&r) == 2 && n) {
-		bool err = false;
+	double d;
+	int cas = 0;
+	while (scanf("%d%lf",&n,&d) == 2 && n) {
+		bool flag = true;
 		for (int i=0;i<n;i++) {
 			double x,y;
 			scanf("%lf%lf",&x,&y);
-			if (y > r) err = true;
-			p[i].first  = x - sqrt(r*r - y*y);
-			p[i].second = x + sqrt(r*r - y*y);
-		}
-		if (err) {
-			printf("Case %d: -1\n",cas++);
-			continue;
+			if (y > d) {
+				flag = false;
+			}
+			double dx = sqrt(d * d - y * y);
+			p[i].second = x - dx;
+			p[i].first = x + dx;
 		}
 		sort(p,p+n);
-		int cnt = 1;
-		pair <double,double> tmp = p[0];
+		if (!flag) {
+			printf("Case %d: -1\n",++cas);
+			continue;
+		}
+		double cur = p[0].first;
+		int ans = 1;
 		for (int i=1;i<n;i++) {
-			if (p[i].first > tmp.second) {
-				cnt ++;
-				tmp = p[i];
-			}
-			else if (p[i].second < tmp.second) {
-				tmp = p[i];
+			if (p[i].second > cur) {
+				cur = p[i].first;
+				ans ++;
 			}
 		}
-		printf("Case %d: %d\n",cas++,cnt);
+		printf("Case %d: %d\n",++cas,ans);
 	}
 	return 0;
 }
