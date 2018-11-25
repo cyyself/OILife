f48204ee7510a90cc08c8af151f50a051f39b5bc
Yangyu Chen <cyy@cyyself.name>
2018-11-25 11:51:55 +0800

    Update 2184 - Cow Exhibition.cpp

--- POJ/2184 - Cow Exhibition.cpp
+++ POJ/2184 - Cow Exhibition.cpp
@@ -2,28 +2,27 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-struct cow {
-	int s,f;
-}c[105];
-int n;
-int f[200005];
+int s[105];
+int f[105];
+int dp[2][200005];
 int main() {
+	int n;
 	scanf("%d",&n);
-	for (int i=0;i<n;i++) scanf("%d%d",&c[i].s,&c[i].f);
-	int center = 1000 * n+1;
-	int range = center * 2;
-	memset(f,0x80,sizeof(f));
-	f[center] = 0;
+	for (int i=0;i<n;i++) scanf("%d%d",&s[i],&f[i]);
+	int center = 1000 * n + 1;
+	int range = 2 * 1000 * n + 1;
+	//waste dp[0]
+	memset(dp,0x80,sizeof(dp));
+	dp[1][center] = 0;
+	int ans = 0;
 	for (int i=0;i<n;i++) {
-		if (c[i].s >= 0) {
-			for (int j=range-1;j>=c[i].s;j--) f[j] = max(f[j],f[j-c[i].s]+c[i].f);
-		}
-		else {
-			for (int j=abs(c[i].s);j-c[i].s<range;j++) f[j] = max(f[j],f[j-c[i].s]+c[i].f);
+		int l = max(0,s[i]);//s[i] > 0
+		int r = min(range,(range)+s[i]);// s[i] < 0
+		for (int j=l;j<=r;j++) {
+			dp[i&1][j] = max(dp[(i&1)^1][j],dp[(i&1)^1][j-s[i]]+f[i]);
+			if (j >= center && dp[i&1][j] > 0) ans = max(ans,j-center+dp[i&1][j]);
 		}
 	}
-	int ans = 0;
-	for (int i=center;i<range;i++) if (f[i] > 0) ans = max(ans,i-center+f[i]);
 	printf("%d\n",ans);
 	return 0;
 }
