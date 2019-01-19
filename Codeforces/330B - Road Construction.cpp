66a3a81d6f92a24cbe01d22a3848f928a2ea2f4a
Yangyu Chen <cyy@cyyself.name>
2019-01-19 23:55:55 +0800

    Create 330B - Road Construction.cpp

---
+++ Codeforces/330B - Road Construction.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+int deg[1005];
+int main() {
+	int n,m;
+	scanf("%d%d",&n,&m);
+	for (int i=0;i<m;i++) {
+		int x,y;
+		scanf("%d%d",&x,&y);
+		deg[x] ++;
+		deg[y] ++;
+	}
+	for (int i=1;i<=n;i++) {
+		if (deg[i] == 0) {
+			printf("%d\n",n-1);
+			for (int j=1;j<=n;j++) {
+				if (j == i) continue;
+				printf("%d %d\n",i,j);
+			}
+			break;
+		}
+	}
+	return 0;
+}
