b23b66d4440fdce9720543e6727d61d0d8f7da2a
Yangyu Chen <cyy@cyyself.name>
2019-01-21 23:18:25 +0800

    Create 签到.cpp

---
+++ Others/签到.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int T;
+	scanf("%d",&T);
+	while (T --) {
+		int n;
+		scanf("%d",&n);
+		if (n == 2) printf("2\n");
+		else printf("1\n");
+	}
+	return 0;
+}
