c5659c04f654b6fcbb37cf14751e4c9d83705cd8
Yangyu Chen <cyy@cyyself.name>
2019-03-10 15:41:57 +0800

    Update 1132F - Clear the String.cpp

--- Codeforces/1132F - Clear the String.cpp
+++ Codeforces/1132F - Clear the String.cpp
@@ -8,17 +8,12 @@ int main() {
 	scanf("%d",&n);
 	scanf("%s",s);
 	for (int i=0;i<n;i++) f[i][i] = 1;
-	for (int i=1;i<n;i++) {
-		if (s[i] == s[i-1]) f[i-1][i] = 1;
-		else f[i-1][i] = 2;
-	}
-	for (int len=3;len<=n;len++) {
+	for (int len=2;len<=n;len++) {
 		for (int i=0;i<n;i++) {
 			int j = i + len - 1;
 			if (j >= n) break;
 			for (int m=i;m<j;m++) {
-				if (s[i] == s[j]) f[i][j] = min(f[i][j],f[i][m]+f[m+1][j]-1);
-				else f[i][j] = min(f[i][j],f[i][m]+f[m+1][j]);
+				f[i][j] = min(f[i][j],f[i][m]+f[m+1][j]-(s[i]==s[j]));
 			}
 		}
 	}
