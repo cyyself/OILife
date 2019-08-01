c7241d5068ac47c04e690413e213709c5e4eb2a3
Yangyu Chen <cyy@cyyself.name>
2019-08-02 01:15:32 +0800

    Create 6000. 「网络流 24 题」搭配飞行员.cpp

---
+++ LOJ/6000. 「网络流 24 题」搭配飞行员.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector <int> g[101];
+int pre[101];
+bool vis[101];
+bool dfs(int u) {
+	for (auto v:g[u]) {
+		if (vis[v]) continue;
+		vis[v] = true;
+		if (pre[v] == -1 || dfs(pre[v])) {
+			pre[v] = u;
+			return true;
+		}
+	}
+	return false;
+}
+int m,n;
+void work() {
+	memset(pre,-1,sizeof(pre));
+	int match = 0;
+	for (int i=1;i<=m;i++) {
+		memset(vis,false,sizeof(vis));
+		match += dfs(i);
+	}
+	printf("%d\n",match);
+}
+int main() {
+	scanf("%d%d",&m,&n);
+	int u,v;
+	while (scanf("%d%d",&u,&v) == 2 && u != -1 && v != -1) g[u].push_back(v);
+	work();
+	return 0;
+}
