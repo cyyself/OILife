627144f5c47c6d2c53f10d6aeadf6b99d6418069
Yangyu Chen <cyy@cyyself.name>
2020-01-06 15:23:42 +0800

    Update 1847 - Good Luck in CET-4 Everybody!.cpp

--- HDU/1847 - Good Luck in CET-4 Everybody!.cpp
+++ HDU/1847 - Good Luck in CET-4 Everybody!.cpp
@@ -1,8 +1,15 @@
 #include <bits/stdc++.h>
+using namespace std;
+bool f[1005];
 int main() {
+	for (int i=0;i<=1000;i++) {
+		if (!f[i]) for (int j=1;i+j<=1000;j<<=1) {
+			f[i+j] = true;
+		}
+	}
 	int n;
 	while (scanf("%d",&n) == 1) {
-		if (n % 3) printf("Kiki\n");
+		if (f[n]) printf("Kiki\n");
 		else printf("Cici\n");
 	}
 	return 0;
