f69aecfd6870530a66594e55365fbc5dc6eb3aa6
Yangyu Chen <cyy@cyyself.name>
2020-07-19 01:12:57 +0800

    Update C - Operation Love.cpp

--- nowcoder/Contest/5668 - 2020牛客暑期多校训练营（第三场）/C - Operation Love.cpp
+++ nowcoder/Contest/5668 - 2020牛客暑期多校训练营（第三场）/C - Operation Love.cpp
@@ -1,62 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define PDD pair<double,double>
+#define y first
+#define x second
 int n;
-struct pos {
-	double x,y;
-	friend bool operator < (const pos &a,const pos &b) {
-		return a.y < b.y || (a.y == b.y && a.x < b.x);
+PDD p[105];
+class mystack : public stack<PDD> {
+public:
+	PDD sec() {
+		PDD tmp = top();
+		pop();
+		PDD sec = top();
+		push(tmp);
+		return sec;
 	}
-}p[25];
-struct stack {//不用STL的原因是STL不能输出栈的次顶元素
-	pos a[105];
-	int t;
-	inline void init() {
-		t = -1;
-	}
-	inline void push(pos i) {
-		a[++t] = i;
-	}
-	inline void pop() {
-		t--;
-	}
-	inline pos top() {
-		return a[t];
-	}
-	inline pos sec() {//次顶
-		return a[t-1];
-	}
-	inline bool available() {
-		return t > 0;//至少有两个元素
-	}
-	inline int size() {
-		return t+1;
-	}
-	inline int topindex() {
-		return t;
-	}
-}s;
-inline bool judge(const pos &e,const pos &c,const pos &s) {
+};
+inline bool judge(const PDD &e,const PDD &c,const PDD &s) {
 	return (e.x - s.x) * (c.y - s.y) - (c.x - s.x) * (e.y - s.y) >= 0;
 }
-void graham() {
+void graham(vector <PDD> &res) {
 	sort(p,p+n);
-	s.init();
+	mystack s;
 	for (int i=0;i<min(2,n);i++) s.push(p[i]);
 	if (n > 2) {
 		for (int i=2;i<n;i++) {
-			while (s.available() && judge(p[i],s.top(),s.sec())) s.pop();
+			while (s.size()>=2 && judge(p[i],s.top(),s.sec())) s.pop();
 			s.push(p[i]);
 		}
 		int len = s.size();
 		s.push(p[n-2]);
 		for (int i=n-3;i>=0;i--) {
-			while (s.size() > len && judge(p[i],s.top(),s.sec())) s.pop();
+			while ((int)s.size() > len && judge(p[i],s.top(),s.sec())) s.pop();
 			s.push(p[i]);
 		}
 	}
 	else s.push(p[0]);
+	res.clear();
+	while (!s.empty()) {
+		res.push_back(s.top());
+		s.pop();
+	}
 }
-inline double dis(const pos &a,const pos &b) {
+inline double dis(const PDD &a,const PDD &b) {
 	return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y));
 }
 const double eps = 1e-1;
@@ -66,15 +51,16 @@ int main() {
 	while (T --) {
 		n = 20;
 		for (int i=0;i<n;i++) scanf("%lf%lf",&p[i].x,&p[i].y);
-		graham();
+		vector <PDD> res;
+		graham(res);
 		vector <double> ed;
-		for (int i=0;i<s.size()-1;i++) ed.push_back(dis(s.a[i],s.a[i+1]));
+		for (int i=0;i<(int)res.size()-1;i++) ed.push_back(dis(res[i],res[i+1]));
 		int idx = 0;
-		for (int i=0;i<ed.size();i++) if (abs(ed[i] - 9) < eps) {
+		for (int i=0;i<(int)ed.size();i++) if (abs(ed[i] - 9) < eps) {
 			idx = i;
 			break;
 		}
-		if (abs(ed[(idx+1)%ed.size()] - 8) < eps) printf("right\n");
+		if (abs(ed[(idx-1+ed.size())%ed.size()] - 8) < eps) printf("right\n");
 		else printf("left\n");
 	}
 	return 0;
